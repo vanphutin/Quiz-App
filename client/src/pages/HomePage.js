ce139b43295da8ad5217f2b8a9f1b79bb0f2fc81
vanphutin2004 <vanphutin.contact@gmail.com>
2024-09-01 16:46:41 +0700

    feat: init variable - test app

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,9 +1,30 @@
 import React from "react";
+import { toast } from "react-toastify";
 
 const HomePage = () => {
   return (
     <div>
-      Hello , welcome <p>{"vanphutin"}</p>
+      Hello , welcome{" "}
+      <a href="">
+        <p>{"vanphutin"}</p>
+      </a>
+      <button
+        onClick={() => {
+          toast.success("success");
+        }}
+      >
+        {" "}
+        Click me --------------
+      </button>
+      <div className="card">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat unde
+        ducimus, exercitationem corrupti sunt ipsam adipisci accusantium dolores
+        porro amet recusandae debitis ullam quibusdam laboriosam. Impedit ad
+        quas et architecto?
+      </div>
+      <div className="alert-success">.alert-success</div>
+      <div className="alert-warning">.alert-warning </div>
+      <div className="selected">a</div>
     </div>
   );
 };
--- client/src/routers/index.js
+++ client/src/routers/index.js
@@ -1,9 +1,25 @@
 import { createBrowserRouter, Outlet } from "react-router-dom";
 import LoginPage from "../pages/LoginPage";
 import HomePage from "../pages/HomePage";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import ScrollToTop from "../utils/ScrollToTop";
 const AuthLayout = () => {
   return (
     <>
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+      <ScrollToTop />
       <Outlet />
     </>
   );
