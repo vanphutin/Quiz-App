e34575bc5375a63e94bd0911809a1c035b9d8119
vanphutin2004 <vanphutin.contact@gmail.com>
2024-09-01 08:54:06 +0700

    connection to database - mySQL

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,33 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = 8081;
+const PORT = process.env.PORT || 8080;
+const database = require("./config/database.config");
+
+// Connect to the database and start the server
+database.getConnection((error, connection) => {
+  if (error) {
+    console.error("Error connecting to database:", error);
+    return;
+  }
+
+  console.log(
+    `Connected to MySQL database || Host: ${database.config.connectionConfig.host}, Port: ${database.config.connectionConfig.port}`
+  );
+
+  connection.release();
+
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+  });
+});
+
+// Global error handler (optional but recommended)
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
 
 app.get("/", (req, res) => {
   res.send("Hello world");
-});
-
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
 });
