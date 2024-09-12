d872db194c1ecfccfecf2cb0565e44f46afecdb0
vanphutin2004 <vanphutin.contact@gmail.com>
2024-09-12 15:03:13 +0700

    fix : token exp

--- server/src/api/v1/controllers/quizzes.controller.js
+++ server/src/api/v1/controllers/quizzes.controller.js
@@ -21,3 +21,62 @@ module.exports.getLevels = async (req, res) => {
     });
   }
 };
+
+// Yêu cầu: GET /quizzes?level=easy&page=1&limit=2&sort=asc
+module.exports.getQuizzesLevel = async (req, res) => {
+  const level = req.query.level;
+  const sort = req.query.sort || "asc";
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+  const offset = (page - 1) * limit;
+
+  if (!level) {
+    return res.status(400).json({
+      statusCode: 400,
+      message: "Level query parameter is missing",
+    });
+  }
+
+  const validSortValues = ["asc", "desc"];
+  if (!validSortValues.includes(sort.toLowerCase())) {
+    return res.status(400).json({
+      statusCode: 400,
+      message: "Invalid sort parameter. Use 'asc' or 'desc'.",
+    });
+  }
+
+  try {
+    const quizzesByCategory = await Quizzes.getQuizzesLevel(
+      level,
+      sort,
+      limit,
+      offset
+    );
+    if (quizzesByCategory.length === 0) {
+      return res.status(404).json({
+        statusCode: 404,
+        message: `No quizzes found for level: ${level}`,
+      });
+    }
+    const formattedData = quizzesByCategory.map((item) => ({
+      category_name: item.category_name,
+      quizzes: JSON.parse(`[${item.quizzes}]`),
+    }));
+    res.status(200).json({
+      statusCode: 200,
+      message: `Get quizzes for level: ${level} successful`,
+      data: formattedData,
+      pagination: {
+        currentPage: page,
+        itemsPerPage: limit,
+        totalItems: quizzesByCategory.length,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      statusCode: 500,
+      message: `Error at quizzes: ${error.message}`,
+      name: error.name,
+    });
+  }
+};
--- server/src/api/v1/middlewares/checkPermisson.js
+++ server/src/api/v1/middlewares/checkPermisson.js
@@ -1,10 +1,10 @@
-import Auth from "../models/auth.model";
+const Auth = require("../models/auth.model");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const md5 = require("md5");
 
 //=== PHÂN QUYỀN NGƯỜI DÙNG
-export const checkPermission = async (req, res, next) => {
+module.exports.checkPermission = async (req, res, next) => {
   try {
     // kiểm tra người dùng đăng nhập hay chưa
     const token = req.headers.authorization?.split(" ")[1];
@@ -47,6 +47,39 @@ export const checkPermission = async (req, res, next) => {
       codeStatus: 500,
       message: error.message,
       name: error.name,
+    });
+  }
+};
+
+//KIỂM TRA TOKEN HẾT HẠN
+module.exports.checkToken = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(403).json({
+      codeStatus: 403,
+      message: "Token is required",
+    });
+  }
+
+  try {
+    // Kiểm tra token xem nó có hợp lệ và chưa hết hạn hay không
+    jwt.verify(token, process.env.JWT_SECRET);
+
+    // Nếu token hợp lệ, cho phép tiếp tục
+    next();
+  } catch (error) {
+    if (error.name === "TokenExpiredError") {
+      return res.status(401).json({
+        status: 401,
+        message: "Token expired",
+        code: "TOKEN_EXPIRED",
+      });
+    }
+
+    return res.status(403).json({
+      codeStatus: 403,
+      message: "Invalid token",
     });
   }
 };
