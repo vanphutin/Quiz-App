5db5976c5585521574e693ab65efeb435bd0f2ce
vanphutin2004 <vanphutin.contact@gmail.com>
2024-09-13 09:51:04 +0700

    feat : api get quizz

--- server/src/api/v1/routers/quizzes.router.js
+++ server/src/api/v1/routers/quizzes.router.js
@@ -2,5 +2,6 @@ const express = require("express");
 const router = express.Router();
 const quizzesController = require("../controllers/quizzes.controller");
 router.get("/levels", quizzesController.getLevels);
+router.get("/", quizzesController.getQuizzesLevel);
 
 module.exports = router;
