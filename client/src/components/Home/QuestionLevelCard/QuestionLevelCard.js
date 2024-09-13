71290897b802910baa6f7bce862b8d83dd843ea2
vanphutin2004 <vanphutin.contact@gmail.com>
2024-09-13 09:54:15 +0700

    feat : merge  header-page

--- client/src/components/Home/QuestionLevelCard/QuestionLevelCard.js
+++ client/src/components/Home/QuestionLevelCard/QuestionLevelCard.js
@@ -5,13 +5,19 @@ import "./__QuestionLevelCard.scss";
 import { Link } from "react-router-dom";
 
 const QuestionLevelCard = ({ titleLevel }) => {
+  const level = titleLevel.toLowerCase();
+
   return (
     <div className="question__card">
       <div className="question__card-title">{titleLevel}</div>
       <div className="question__card-icon">
         <LuShieldQuestion fontSize="6rem" />
       </div>
-      <Link state={{ level: titleLevel }}>
+
+      <Link
+        to={`/quiz?level=${level}`}
+        state={{ titleLevel }} // This is how you set the state properly
+      >
         <button className="question__card-button">
           Get started <BsArrowUpRight className="button-icon" />
         </button>
