2920acb8c4a60a434cad76688368ef06f0d8235e
vanphutin2004 <vanphutin.contact@gmail.com>
2024-11-23 16:30:00 +0700

    fix

--- src/api/v1/routers/answer.router.js
+++
@@ -1,5 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const answerController = require("../controllers/answer.controller");
-router.post("/", answerController.postNewAnswer);
-module.exports = router;
--- src/api/v1/routers/ranking.router.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const rankingController = require("../controllers/ranking.controller");
-
-router.get("/:id", rankingController.getRanking);
-
-module.exports = router;
